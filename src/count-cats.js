47d51f99e461009d65222b555cb9c57aeb5a9a4a
Pavel Niakhai <3055963@mail.ru>
2020-10-10 16:28:32 +0300

    docs: add cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,6 +2,5 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   return matrix.map((subMatrix) => subMatrix.filter((item) => item === '^^')).flat().length;
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  
 };
